4b6174be4812c7aa378fe8ea359adc97d87c100d
ciceiOSAndroid <39631775+ciceiOSAndroid@users.noreply.github.com>
2018-05-25 20:31:14 +0200

    Update ViewController.swift

--- EdadPerro/ViewController.swift
+++ EdadPerro/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("Empezamos con Git")
+        print("Buen trabajo")
     }
 
     override func didReceiveMemoryWarning() {
