a05a75d0a73e7aaf976bd4f1bc656c4709dcef22
CICE <cice@aula03formador.local>
2018-05-25 20:34:54 +0200

    Commit pantalla_inicio - Modificaciones viewDidLoad()

--- EdadPerro/ViewController.swift
+++ EdadPerro/ViewController.swift
@@ -12,15 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
         print("Empezamos con Git")
-        print("Buen trabajo")
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        print("Muy buen trabajo")
     }
 }
 
